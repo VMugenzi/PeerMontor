cd5f3f2f8cd1ba64339fc665001608fd3fd559cb
VMugenzi <84989141+VMugenzi@users.noreply.github.com>
2021-07-23 07:57:38 +0200

    This is Database connected

--- dist/server.dev.js
+++ dist/server.dev.js
@@ -7,10 +7,30 @@ exports["default"] = void 0;
 
 var _express = _interopRequireDefault(require("express"));
 
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+_dotenv["default"].config({
+  path: './.env'
+});
+
 var app = (0, _express["default"])();
+var databaseUrl = process.env.DATABASE;
+
+_mongoose["default"].connect(databaseUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+}).then(function () {
+  return console.log("Database connected sucessfully");
+});
+
 app.listen(4040, function () {
+  //console.log(databaseUrl)
   console.log('Server is running on port 4040');
 });
 var _default = app;
